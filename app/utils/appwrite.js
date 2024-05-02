9ad89937a58a6aacad8c2fc3526de8d9012c3644
fifartdev <fifartapp@gmail.com>
2024-05-02 09:55:54 +0300

    env added

--- app/utils/appwrite.js
+++ app/utils/appwrite.js
@@ -3,8 +3,8 @@ import { Client, Account, ID, Query } from "appwrite";
 const client = new Client();
 
 client
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("66320add0031254e66bb");
+  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT);
 
 const account = new Account(client);
 
